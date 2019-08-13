c9a4888357922034daa9a576763187965913cc54
tingting76319 <tingting76319@gmail.com>
2019-08-14 00:30:43 +0800

    Init hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg950\cocoartf1561\cocoasubrtf600
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red26\green26\blue26;\red245\green249\blue250;\red210\green0\blue53;
+}
+{\*\expandedcolortbl;;\cssrgb\c13333\c13333\c13333;\cssrgb\c96863\c98039\c98431;\cssrgb\c86667\c6667\c26667;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl260\partightenfactor0
+
+\f0\fs21\fsmilli10800 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 print(\cf4 \strokec4 'hello python & git'\cf2 \strokec2 )}
